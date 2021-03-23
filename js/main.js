65130ab85e6583af56f679346380d1337eec5654
Milessa <milessacroitor@gmail.com>
2021-03-23 15:12:24 +0200

    create: activate the slider with arrows

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ const swiper = new Swiper(".swiper-container", {
   },
   speed: 600,
 });
+
+// control slider with arrows
+document.onkeydown = function (event) {
+  if (event.keyCode == 37) {
+    // left arrow
+    $(".slider-button--prev").click();
+  } else if (event.keyCode == 39) {
+    // right arrow
+    $(".slider-button--next").click();
+  } else if (event.keyCode == 38) {
+    // right arrow
+    alert(
+      "Yuppie, congratulations you found the secret message! Type as answer 'I did it'"
+    );
+  }
+};
