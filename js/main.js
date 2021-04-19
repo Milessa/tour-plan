27814325117636bb50c40075ead060c35fe091d5
Milessa <milessacroitor@gmail.com>
2021-04-19 23:34:54 +0300

    fix: maps changed from Yandex to Google & PageSpeed+

--- js/main.js
+++ js/main.js
@@ -1,137 +1,65 @@
+document.addEventListener("touchstart", onTouchStart, { passive: true });
 $(document).ready(function () {
-  // slider hotel section
-  const hotelSlider = new Swiper(".hotel-slider", {
-    // Optional parameters
-    loop: true,
-    // Navigation arrows
+  new Swiper(".hotel-slider", {
+    loop: !0,
     navigation: {
       nextEl: ".hotel-slider__button--next",
       prevEl: ".hotel-slider__button--prev",
     },
     speed: 600,
-
-    // control slider with arrows
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-  });
-
-  // slider reviews section
-  const reviewsSlider = new Swiper(".reviews-slider", {
-    // Optional parameters
-    loop: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: ".reviews-slider__button--next",
-      prevEl: ".reviews-slider__button--prev",
-    },
-    speed: 600,
-
-    // control slider with arrows
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-  });
-
-  // code for map
-  ymaps.ready(init);
-
-  function init() {
-    var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [7.89082817, 98.29456143],
-        zoom: 15,
-        controls: [],
+    keyboard: { enabled: !0, onlyInViewport: !1 },
+  }),
+    new Swiper(".reviews-slider", {
+      loop: !0,
+      navigation: {
+        nextEl: ".reviews-slider__button--next",
+        prevEl: ".reviews-slider__button--prev",
       },
-      {
-        searchControlProvider: "yandex#search",
-        draggable: true,
-      }
-    );
-
-    myMap.geoObjects.add(
-      new ymaps.Placemark(
-        [7.89082817, 98.29456143],
-        {
-          iconCaption: "Grande Hilton Hotel",
-        },
-        {
-          iconColor: "#EC1F46",
-        }
-      )
-    );
-  }
-
-  // code mobile menu button
-  var menuButton = $(".menu-button");
-  menuButton.on("click", function () {
+      speed: 600,
+      keyboard: { enabled: !0, onlyInViewport: !1 },
+    });
+  $(".menu-button").on("click", function () {
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   });
-
-  // code for modal window
-  var modalButton = $('[data-toggle="modal"]');
-  var closeModalButton = $(".modal__close");
-  modalButton.on("click", openModal);
-  closeModalButton.on("click", closeModal);
-
-  function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass("modal__overlay--visible");
-    modalDialog.addClass("modal__dialog--visible");
-  }
-
-  function closeModal(event) {
-    event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-  }
-
-  // code for closign modal window by pressing Esc
-  $(document).keyup(function (event) {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    if (event.key == "Escape") {
-      modalOverlay.removeClass("modal__overlay--visible");
-      modalDialog.removeClass("modal__dialog--visible");
-    }
-  });
-
-  //code for phone mask
-  $(".phone-mask").inputmask("+7(999)999-99-99");
-  //code for modal form processing
-  $(".form").each(function () {
-    $(this).validate({
-      messages: {
-        name: {
-          minlength: "Name should be at least 3 symbols",
+  var e = $('[data-toggle="modal"]'),
+    a = $(".modal__close");
+  e.on("click", function () {
+    var e = $(".modal__overlay"),
+      a = $(".modal__dialog");
+    e.addClass("modal__overlay--visible"), a.addClass("modal__dialog--visible");
+  }),
+    a.on("click", function (e) {
+      e.preventDefault();
+      var a = $(".modal__overlay"),
+        o = $(".modal__dialog");
+      a.removeClass("modal__overlay--visible"),
+        o.removeClass("modal__dialog--visible");
+    }),
+    $(document).keyup(function (e) {
+      var a = $(".modal__overlay"),
+        o = $(".modal__dialog");
+      "Escape" == e.key &&
+        (a.removeClass("modal__overlay--visible"),
+        o.removeClass("modal__dialog--visible"));
+    }),
+    $(".phone-mask").inputmask("+7(999)999-99-99"),
+    $(".form").each(function () {
+      $(this).validate({
+        messages: {
+          name: { minlength: "Name should be at least 3 symbols" },
+          email: {
+            email:
+              "Your email address must be in the format of name@domain.com",
+          },
         },
-        email: {
-          email: "Your email address must be in the format of name@domain.com",
-        },
-      },
+      });
     });
+}),
+  $.validator.addMethod("checkMaskPhone", function (e, a) {
+    return /\+\d{1}\(\d{3}\)\d{3}-\d{2}-\d{2}/g.test(e);
+  }),
+  $(".form").each(function () {
+    $(this).validate(),
+      $.validator.addClassRules({ "phone-mask": { checkMaskPhone: !0 } }),
+      AOS.init();
   });
-});
-
-// rule for phone validation
-$.validator.addMethod("checkMaskPhone", function (value, element) {
-  return /\+\d{1}\(\d{3}\)\d{3}-\d{2}-\d{2}/g.test(value);
-});
-// take our form by class
-$(".form").each(function () {
-  $(this).validate();
-  // put validation for phone input by class
-  $.validator.addClassRules({
-    "phone-mask": {
-      checkMaskPhone: true,
-    },
-  });
-
-  AOS.init();
-});
